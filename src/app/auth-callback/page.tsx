f9e69fa9915316ca27c758ad29337ebfd47ec2da
harkanni <harkanzyempire001@gmail.com>
2024-12-31 01:35:45 +0100

    TRPC SETUP

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+const page = () => {
+   const router = useRouter()
+
+   const searchParams = useSearchParams()
+   const origin = searchParams.get('origin')
+
+
+    
+   
+}
+
+export default page
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = () => {
-  return (
-    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
-      <MaxWidthWrapper>
-         <div className='flex h-14 items-center justify-between border-b border-r-zinc-200'>
-            <Link href={'/'} className='flex z-40 font-semibold'>
-               <span>Chad.</span>
-            </Link>
-         </div>
-      </MaxWidthWrapper>
-    </nav>
-  )
+   return (
+      <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+         <MaxWidthWrapper>
+            <div className='flex h-14 items-center justify-between border-b border-r-zinc-200'>
+               <Link href={'/'} className='flex z-40 font-semibold'>
+                  <span>Chad.</span>
+               </Link>
+
+               {/* todo: add mobile navbar */}
+
+               <div className='hidden items-center space-x-4 sm:flex'>
+                  <>
+                     <Link href={'/pricing'} className={buttonVariants({
+                        variant: 'ghost',
+                        size: 'sm', className: 'font-semibold'
+                     })}>Pricing</Link>
+
+                     <LoginLink className={buttonVariants({
+                        variant: 'ghost',
+                        size: 'sm', className: 'font-semibold'
+                     })}>
+                        Sign In
+                     </LoginLink>
+                  </>
+               </div>
+            </div>
+
+         </MaxWidthWrapper>
+      </nav>
+   )
 }
 
 export default Navbar
