1d7b8183c94db2b10997e407ca14583d7b8ecb9d
harkanni <harkanzyempire001@gmail.com>
2025-01-06 03:11:30 +0100

    feat: update UI library, fix file upload, and add chat page redirection
    - Updated shadcn to include the latest progress.tsx UI improvements.
    - Fixed issues with file uploads to UploadThing.
    - Added UI features to indicate redirection to the chat page after successful file upload.

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/button'
 import { DialogTitle } from '@radix-ui/react-dialog'
 
 import Dropzone from "react-dropzone"
-import { Cloud, File } from 'lucide-react'
+import { Cloud, File, Loader2 } from 'lucide-react'
 import { Progress } from '@/components/ui/progress'
 import { useUploadThing } from '@/lib/uploadthing'
 import { useToast } from '@/hooks/use-toast'
@@ -17,7 +17,7 @@ import { useRouter } from 'next/navigation'
 const UploadDropzone = () => {
    const router = useRouter()
 
-   const [isUploading, setIsUploading] = useState<boolean>(true)
+   const [isUploading, setIsUploading] = useState<boolean>(false)
    const [uploadProgress, setUploadProgress] = useState<number>(0)
    const { toast } = useToast()
 
@@ -98,6 +98,7 @@ const UploadDropzone = () => {
 
          console.log("This is the key:.........", key);
 
+         // @ts-ignore
          startPolling({ key })
 
          // setIsUploading(false)
@@ -132,7 +133,15 @@ const UploadDropzone = () => {
 
                      {isUploading ? (
                         <div className='w-full mt-4 max-w-xs mx-auto'>
-                           <Progress className='h-1 w-full bg-zinc-200' value={uploadProgress} />
+                           <Progress indicatorColor={
+                              uploadProgress === 100 ? 'bg-green-200' : ''
+                           } className='h-1 w-full bg-zinc-200' value={uploadProgress} />
+
+                           {uploadProgress === 100 ? (
+                              <div className='flex gap-1 items-center justify-center text-sm text-center pt-2'>
+                                 <Loader2 className='h-3 w-3 animate-spin' /> Redirecting.....
+                              </div>
+                           ) : null}
                         </div>
                      ) : null}
 
