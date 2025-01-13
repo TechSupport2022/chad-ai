9de69b6f50b7beb361cd7663d1c829ea91b0cf4f
harkanni <harkanzyempire001@gmail.com>
2025-01-13 08:45:37 +0100

    PDF Renderer Functionality

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+import { useToast } from '@/hooks/use-toast';
+import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { Document, Page, pdfjs } from "react-pdf"
+import { useResizeDetector } from "react-resize-detector"
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
    'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -16,8 +21,14 @@ interface PDFRendererProps {
    url: string;
 }
 const PdfRenderer = ({ url }: PDFRendererProps) => {
-   console.log(`PDFRenderer url is hereee........: ${url}`);
    const [pdfBlob, setPdfBlob] = useState<string | null>(null);
+
+   const { toast } = useToast()
+
+   const { width, ref } = useResizeDetector()
+
+   const [numPages, setNumPages] = useState<number>()
+   const [currPage, setCurrPage] = useState<number>(1)
 
    useEffect(() => {
       const fetchPDF = async () => {
@@ -37,14 +48,56 @@ const PdfRenderer = ({ url }: PDFRendererProps) => {
    return (
       <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
          <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-            <div className='flex items-center gap-1.5'></div>
+            <div className='flex items-center gap-1.5'>
+               <Button variant={'ghost'} aria-label='previous page'
+                  disabled={currPage <= 1}
+                  onClick={() => {
+                     setCurrPage((prev) => (prev - 1 > 1 ? prev-- : 1))
+                  }}>
+                  <ChevronDown className='h-4 w-4' />
+               </Button>
+
+               <div className='flex items-center gap-1.5'>
+                  <Input className="w-12 h-7" defaultValue={1} />
+                  <p className='text-zinc-700 text-sm space-x-1'>
+                     <span>/</span>
+                     <span>{numPages ?? 'x'}</span>
+                  </p>
+               </div>
+
+               <Button
+                  variant={'ghost'}
+                  aria-label='previous page'
+                  disabled={numPages === undefined || numPages === currPage}
+                  onClick={() => {
+                     setCurrPage((prev) => prev + 1 > numPages! ? numPages! : prev++)
+                  }}
+               >
+                  <ChevronUp className='h-4 w-4' />
+               </Button>
+            </div>
          </div>
 
          <div className='flex-1 w-full max-h-screen'>
-            <div>
-               {/* <Document className='max-h-full' file={'/pd.pdf'}> */}
-               <Document className='max-h-full' file={url}>
-                  <Page pageNumber={1} />
+            <div ref={ref}>
+               <Document
+                  className='max-h-full'
+                  file={url}
+                  loading={
+                     <div className='flex justify-center'>
+                        <Loader2 />
+                     </div>
+                  }
+                  onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                  onLoadError={() =>
+                     toast({
+                        title: 'Error loading',
+                        description: 'Please try again later.',
+                        variant: 'destructive'
+                     })
+                  }
+               >
+                  <Page width={width ? width : 1} pageNumber={currPage} />
                </Document>
             </div>
          </div>
