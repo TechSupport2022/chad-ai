651aa253e9787af298361bbf209017ce25250243
harkanni <harkanzyempire001@gmail.com>
2025-01-16 04:11:10 +0100

    feat: Added PDF full screen mode. Added rotate pdf functionality ui changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 
-
 import "react-loading-skeleton/dist/skeleton.css"
+import "simplebar-react/dist/simplebar.min.css"
 import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
