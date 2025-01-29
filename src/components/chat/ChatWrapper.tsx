c33db45fd2b3e3d762834b424c8f37bdc8e5223f
harkanni <harkanzyempire001@gmail.com>
2025-01-29 22:38:17 +0100

    add: chat input section

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 import Messages from './Messages'
 import ChatInput from './ChatInput'
 import { trpc } from '@/app/_trpc/client'
+import { ChevronLeft, Loader2, XCircle } from 'lucide-react'
+import Link from 'next/link'
+import { buttonVariants } from '../ui/button'
 
 interface ChatWrapperProps {
    fileId: string;
@@ -12,23 +15,81 @@ interface ChatWrapperProps {
 
 const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
 
-   const { data }: any = trpc.getFileUploadtStatus.useQuery({ fileId }, {
+   const { data, isLoading } = trpc.getFileUploadtStatus.useQuery({ fileId }, {
       retry: true,
       retryDelay: 500,
-      refetchInterval: () => {
-         return data?.status === "SUCCESS" ||
-         data?.status === "FAILED" ?
-         false : 500;
+      refetchInterval: (queryData) => {
+         //@ts-ignore
+         return queryData?.status === "SUCCESS" ||
+            //@ts-ignore
+            queryData?.status === "FAILED" ?
+            false : 500;
       }
    })
 
-   console.log("This is data and fileId:........", data, fileId)
+   if (isLoading) {
+      return <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+         <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+            <div className='flex flex-col items-center gap-2'>
+               <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
+               <h3 className='font-semibold text-xl'>Loading...</h3>
+               <p className='text-zinc-500 text-sm'>
+                  We&apos;re preparing your PDF.
+               </p>
+            </div>
+         </div>
+      </div>
+   }
 
-   // if (queryData?.status === "SUCCESS") {
-   //    console.log("File upload successful!");
-   // } else if (queryData?.status === "FAILED") {
-   //    console.log("File upload failed.");
-   // }
+   if (data?.status === "PROCESSING") {
+      return (
+         <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+            <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+               <div className='flex flex-col items-center gap-2'>
+                  <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
+                  <h3 className='font-semibold text-xl'>
+                     Processing PDF...
+                  </h3>
+                  <p className='text-zinc-500 text-sm'>
+                     This Won&apos;t take long.
+                  </p>
+               </div>
+            </div>
+
+            <ChatInput isDisabled />
+         </div>
+      )
+   }
+
+   if (data?.status === "FAILED") {
+      return (
+         <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+            <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+               <div className='flex flex-col items-center gap-2'>
+                  <XCircle className='h-8 w-8 text-red-500 animate-bounce' />
+                  <h3 className='font-semibold text-xl'>
+                     Too many pages in PDF...
+                  </h3>
+                  <p className='text-zinc-500 text-sm'>
+                     <span className='font-medium'>Free</span>{' '}
+                     plan supports up to 5 pages per PDF.
+                  </p>
+                  <Link href="/dashboard" className={buttonVariants({
+                     variant: "secondary",
+                     className: "mt-4"
+                  })}>
+                     <ChevronLeft className='h-3 w-3 mr-1.5' />
+                     Back
+                  </Link>
+               </div>
+            </div>
+
+            <ChatInput isDisabled />
+         </div>
+      )
+   }
+
+   console.log("This is data and fileId:........", data, fileId)
 
 
 
@@ -39,7 +100,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
             <Messages />
          </div>
 
-         <ChatInput />
+         <ChatInput isDisabled={isLoading} />
       </div>
    )
 }
