8a680f5738faeae6925cbe3da7e05bb802032154
harkanni <harkanzyempire001@gmail.com>
2025-02-13 13:48:38 +0100

    modified next config to ignore ts errors

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const nextConfig = {
+   /* config options here */
+   webpack: (config) => {
+      config.resolve.alias.canvas = false
+      config.resolve.alias.encoding = false
+      return config
+   },
+   typescript: {
+      ignoreBuildErrors: true,
+   },
+   eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+   },
+};
+
+export default nextConfig;
