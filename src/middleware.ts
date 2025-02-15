fcf9117ed4dbfad585e52ba0afda4f9fd9d2d9fc
harkanni <harkanzyempire001@gmail.com>
2025-02-15 05:19:44 +0100

    updated trpc to prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,8 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const config = {
   matcher: ['/dashboard/:path*', '/auth-callback'],
 }
-
-// export default authMiddleware
-
 
 
 export default function middleware(req: any) {
