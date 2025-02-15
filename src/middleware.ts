5186bbec0f3eb32c14da7a7fead356f823823f4e
harkanni <harkanzyempire001@gmail.com>
2025-02-15 04:57:07 +0100

    feat: added middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const config = {
   matcher: ['/dashboard/:path*', '/auth-callback'],
 }
 
-export default authMiddleware
+// export default authMiddleware
+
+
+
+export default function middleware(req: any) {
+  return withAuth(req);
+}
