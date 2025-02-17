a651a8df763a644382ebb2428a62341f8abb7356
harkanni <harkanzyempire001@gmail.com>
2025-02-17 08:13:11 +0100

    moving from kinde to clerk auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/', 'dashboard']);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
