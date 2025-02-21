0b91471340ed0c27a5881056faaa4393585885cb
harkanni <harkanzyempire001@gmail.com>
2025-02-21 16:18:30 +0100

    fix: mobile navbar responsive to auth state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,21 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
 import { ArrowRight } from 'lucide-react'
+import MobileNav from './MobileNav'
+import { currentUser } from '@clerk/nextjs/server'
 
-const Navbar = () => {
+const Navbar = async () => {
+   const authUser = await currentUser()
+
    return (
       <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
          <MaxWidthWrapper>
-            <div className='flex h-14 items-center justify-between border-b border-r-zinc-200'>
+            <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
                <Link href={'/'} className='flex z-40 font-semibold'>
                   <span>Chad.</span>
                </Link>
 
+               <MobileNav isAuth={!!authUser} />
                {/* todo: add mobile navbar */}
 
                <div className='hidden items-center space-x-4 sm:flex'>
